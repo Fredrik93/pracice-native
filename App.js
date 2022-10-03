749165f47e247d0da780c4d1d810c667297a63e2
Fredrik93 <fredrikullman93@gmail.com>
2022-10-03 11:06:18 +0200

    update to run on android

--- App.js
+++ App.js
@@ -1,26 +1,18 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import ComponentsScreen from './src/ComponentScreen';
-// function HomeScreen() {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>Home Screen</Text>
-//     </View>
-//   );
-// }
 
-const Stack = createNativeStackNavigator();
+const navigator = createStackNavigator(
+  {
+    Home: ComponentsScreen,
+    Components: ComponentsScreen,
+  },
+  {
+    initialRouteName: 'Components',
+    defaultNavigationOptions: {
+      title: 'Home',
+    },
+  }
+);
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home Page" component={ComponentsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
+export default createAppContainer(navigator);
