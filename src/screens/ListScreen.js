60fb7a42d0f0e5e5c62321dacf816880703d83de
Fredrik93 <fredrikullman93@gmail.com>
2022-10-10 09:34:15 +0200

    add list prop

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,25 +4,25 @@ import { View, Text, StyleSheet, FlatList } from 'react-native'
 
 const ListScreen = () => {
     const friends = [
-        { name: "Friend#1" },
-        { name: "Friend#2" },
-        { name: "Friend#3" },
-        { name: "Friend#4" },
-        { name: "Friend#5" },
-        { name: "Friend#6" },
-        { name: "Friend#7" },
-        { name: "Friend#8" },
-        { name: "Friend#9" }
+        { name: "Friend#1", age: 20 },
+        { name: "Friend#2", age: 45 },
+        { name: "Friend#3", age: 32 },
+        { name: "Friend#4", age: 27 },
+        { name: "Friend#5", age: 53 },
+        { name: "Friend#6", age: 30 },
+        { name: "Friend#7", age: 12 },
+        { name: "Friend#8", age: 65 },
+        { name: "Friend#9", age: 33 }
     ]
     return (
         <FlatList
             data={friends}
-            horizontal
+            horizontal={false}
             showsHorizontalScrollIndicator={false}
             keyExtractor={(friend) => friend.name}
             //you can destructure this and write renderItem=({item}) => .. instead
             renderItem={(elem) => {
-                return <Text style={styles.textStyle} > {elem.item.name}</Text>
+                return <Text style={styles.textStyle} > {elem.item.name} - Age: {elem.item.age}</Text>
             }} />)
 }
 
