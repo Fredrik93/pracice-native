450bad0438aba048dc288cdb5a708c1b0229e98c
Fredrik93 <fredrikullman93@gmail.com>
2022-10-18 07:53:52 +0200

    add button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { View, Button, Text, StyleSheet } from 'react-native'
 import { clickProps } from 'react-native-web/dist/cjs/modules/forwardedProps'
 
-const HomeScreen = (props) => {
-    return <Text> {props.homeText} </Text>
+const HomeScreen = () => {
+    return <View> 
+        <Text style={styles.text} > {"Home Screen"} </Text>
+        <Button
+        onPress={()=> console.log("Button Pressed")}
+        title='Go to Components Demo' />
+         </View> 
 }
 
-
+const styles = StyleSheet.create({
+    text:{
+        fontSize:30
+    }
+})
 
 export default HomeScreen
