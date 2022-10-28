5d9e12cb2fcb34302ab35ff73e4f71b7910f4881
Fredrik93 <fredrikullman93@gmail.com>
2022-10-28 15:04:09 +0200

    destructure navigation props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import { View, Button, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import { clickProps } from 'react-native-web/dist/cjs/modules/forwardedProps'
 
-const HomeScreen = (props) => {
-    return <View> 
+const HomeScreen = ({ navigation }) => {
+    return <View>
         <Text style={styles.text} > {"Home Screen"} </Text>
         <Button
-        onPress={()=> props.navigation.navigate("Components")}
-        title='Go to Components Demo' />
+            onPress={() => navigation.navigate("Components")}
+            title='Go to Components Demo' />
+        <View style={styles.space} />
+        <Button title='Go to List Demo'
+            onPress={() => { navigation.navigate('List') }}
+        />
+        <View style={styles.space} />
 
         <TouchableOpacity
-        onPress={()=>{console.log("List Pressed")}}
+            onPress={() => { navigation.navigate('List') }}
         >
             <Text>Go To List Demo</Text>
-            </TouchableOpacity>      
-         </View> 
+        </TouchableOpacity>
+    </View>
 }
 
 const styles = StyleSheet.create({
-    text:{
-        fontSize:30
-    }
+    text: {
+        fontSize: 30
+    },
+    space: {
+        width: 20, // or whatever size you need
+        height: 20,
+    },
 })
 
 export default HomeScreen
