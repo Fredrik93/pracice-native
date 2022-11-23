7b01fce6e1183f84e8c1ea56962c4955d737b770
Fredrik93 <fredrikullman93@gmail.com>
2022-11-23 10:03:35 +0100

    test

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text } from 'react-native'
 const ImageScreen = () => {
-    return (<Text>Images Screen</Text>)
+    return (<Text>Image Screen</Text>)
 }
 
 const styles = StyleSheet.create({})
