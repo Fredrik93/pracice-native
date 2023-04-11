9713b9c9ab15821e4582f020d19f5560404ec5ff
Fredrik93 <fredrikullman93@gmail.com>
2023-04-11 10:05:49 +0200

    add view for color box

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -4,13 +4,25 @@ import {View,Text,StyleSheet} from 'react-native'
 import ColorCounter from '../components/ColorCounter'
 
 const SquareScreen = () => {
+    const CHANGE_RGB_COLOR =15;
     const [red,setRed] = useState(0)
-console.log(red)
+    const [blue, setBlue] = useState(0)
+    const [green,setGreen] = useState(0)
     return (
         <View>
-            <ColorCounter  color="Red" onIncrease={()=> setRed(red+1)} onDecrease={()=> setRed(red-1)}/>
-            <ColorCounter  color="Blue" />
-            <ColorCounter color="Green" />
+            <ColorCounter  
+            color="Red" 
+            onIncrease={()=> setRed(red+CHANGE_RGB_COLOR)} 
+            onDecrease={()=> setRed(red-CHANGE_RGB_COLOR)}/>
+            <ColorCounter  color="Blue"
+            onIncrease={()=>{setBlue(blue+CHANGE_RGB_COLOR)}}
+            onDecrease={()=>{setBlue(blue-CHANGE_RGB_COLOR)}}
+            />
+            <ColorCounter color="Green" 
+            onIncrease={()=>{setGreen(green+CHANGE_RGB_COLOR)}}
+            onDecrease={()=>{setGreen(green-CHANGE_RGB_COLOR)}}
+            />
+            <View style={{height:100,width:100, backgroundColor:`rgb(${red},${green},${blue})`}} ></View>
         </View>
     )
 }
