5d536e65ee4866a6db5993ce9998c01989f7a0f9
Fredrik93 <fredrikullman93@gmail.com>
2023-04-18 14:14:41 +0200

    add setColor to all rgb colors

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 import { View, Text, StyleSheet } from 'react-native'
 import ColorCounter from '../components/ColorCounter'
+import { log } from 'react-native-reanimated';
 
 const SquareScreen = () => {
     const CHANGE_RGB_COLOR = 15;
@@ -10,11 +11,24 @@ const SquareScreen = () => {
     const [green, setGreen] = useState(0)
 
     const setColor = (color, change) => {
-        if (color === 'red') {
-            if (red + change > 255 || red + change < 0) {
+        switch (color) {
+            case 'red':
+                console.log(red);
+                red + change > 255 || red + change < 0 ? null : setRed(red + change)
                 return;
-            } else { setRed(red + change) }
+            case 'blue':
+                console.log(blue);
+                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
+                return;
+            case 'green':
+                console.log(green);
+                green + change > 255 || green + change < 0 ? null : setGreen(green + change)
+                return;
+
+            default:
+                break;
         }
+
     }
     return (
         <View>
@@ -23,12 +37,12 @@ const SquareScreen = () => {
                 onIncrease={() => setColor('red', CHANGE_RGB_COLOR)}
                 onDecrease={() => setColor('red', -1 * CHANGE_RGB_COLOR)} />
             <ColorCounter color="Blue"
-                onIncrease={() => { setBlue(blue + CHANGE_RGB_COLOR) }}
-                onDecrease={() => { setBlue(blue - CHANGE_RGB_COLOR) }}
+                onIncrease={() => { setColor('blue', CHANGE_RGB_COLOR) }}
+                onDecrease={() => { setColor('blue', -1 * CHANGE_RGB_COLOR) }}
             />
             <ColorCounter color="Green"
-                onIncrease={() => { setGreen(green + CHANGE_RGB_COLOR) }}
-                onDecrease={() => { setGreen(green - CHANGE_RGB_COLOR) }}
+                onIncrease={() => { setColor('green', CHANGE_RGB_COLOR) }}
+                onDecrease={() => { setColor('green', -1 * CHANGE_RGB_COLOR) }}
             />
             <View style={{ height: 100, width: 100, backgroundColor: `rgb(${red},${green},${blue})` }} ></View>
         </View>
