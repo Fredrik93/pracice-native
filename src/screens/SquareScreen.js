0302ed5ef23a83e3597a83d38695414e5b907c87
Fredrik93 <fredrikullman93@gmail.com>
2023-04-20 14:56:54 +0200

    update to community convention for reducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -6,20 +6,21 @@ const CHANGE_RGB_COLOR = 15;
 
 const reducer = (state, action) => {
     //State is === {red: number, green:number, blue:number}
-    //action is === {colorToChange:'red' ||'green||'blue, amount: 15 || -15}
+    //action is === {type:'change_red' ||'change_green||'change_blue, payload: 15 || -15}
 
-    switch (action.colorToChange) {
+    switch (action.type) {
         //stopped here. you are removing state hooks and replacing them with a reducer. a reducer is useful for updating object state, just like hooks.
         // a reducer is just a function that changes state of an object.
-        case 'red':
-            return state.red + action.amount > 255 || state.red + action.amount < 0
-                ? state : { ...state, red: state.red + action.amount }
-        case 'green':
-            return state.green + action.amount > 255 || state.green + action.amount < 0
-                ? state : { ...state, green: state.green + action.amount }
-        case 'blue':
-            return state.blue + action.amount > 255 || state.blue + action.amount < 0
-                ? state : { ...state, blue: state.blue + action.amount }
+        case 'change_red':
+            return state.red + action.payload > 255 || state.red + action.payload < 0
+                //if true then return the old state, otherwise update it 
+                ? state : { ...state, red: state.red + action.payload }
+        case 'change_green':
+            return state.green + action.payload > 255 || state.green + action.payload < 0
+                ? state : { ...state, green: state.green + action.payload }
+        case 'change_blue':
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0
+                ? state : { ...state, blue: state.blue + action.payload }
         default:
             return state;
     }
@@ -34,15 +35,15 @@ const SquareScreen = () => {
         <View>
             <ColorCounter
                 color="Red"
-                onIncrease={() => dispatch({ colorToChange: 'red', amount: CHANGE_RGB_COLOR })}
-                onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * CHANGE_RGB_COLOR })} />
+                onIncrease={() => dispatch({ type: 'change_red', payload: CHANGE_RGB_COLOR })}
+                onDecrease={() => dispatch({ type: 'change_red', payload: -1 * CHANGE_RGB_COLOR })} />
             <ColorCounter color="Blue"
-                onIncrease={() => dispatch({ colorToChange: 'blue', amount: CHANGE_RGB_COLOR })}
-                onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * CHANGE_RGB_COLOR })}
+                onIncrease={() => dispatch({ type: 'change_blue', payload: CHANGE_RGB_COLOR })}
+                onDecrease={() => dispatch({ type: 'change_blue', payload: -1 * CHANGE_RGB_COLOR })}
             />
             <ColorCounter color="Green"
-                onIncrease={() => dispatch({ colorToChange: 'green', amount: CHANGE_RGB_COLOR })}
-                onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * CHANGE_RGB_COLOR })}
+                onIncrease={() => dispatch({ type: 'change_green', payload: CHANGE_RGB_COLOR })}
+                onDecrease={() => dispatch({ type: 'change_green', payload: -1 * CHANGE_RGB_COLOR })}
             />
             <View style={{ height: 100, width: 100, backgroundColor: `rgb(${red},${green},${blue})` }} ></View>
         </View>
