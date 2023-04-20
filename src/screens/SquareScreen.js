8b8189535b68497d33d8f9095babc533f739fe34
Fredrik93 <fredrikullman93@gmail.com>
2023-04-20 14:12:52 +0200

    replace hooks with reducer concept

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,48 +1,45 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 
 import { View, Text, StyleSheet } from 'react-native'
 import ColorCounter from '../components/ColorCounter'
-import { log } from 'react-native-reanimated';
+const CHANGE_RGB_COLOR = 15;
+
+const reducer = (state, action) => {
+    //State is === {red: number, green:number, blue:number}
+    //action is === {colorToChange:'red' ||'green||'blue, amount: 15 || -15}
+
+    switch (action.colorToChange) {
+        //stopped here. you are removing state hooks and replacing them with a reducer. a reducer is useful for updating object state, just like hooks.
+        // a reducer is just a function that changes state of an object.
+        case 'red':
+            return { ...state, red: state.red + action.amount }
+        case 'green':
+            return { ...state, green: state.green + action.amount }
+        case 'blue':
+            return { ...state, blue: state.blue + action.amount }
+        default:
+            return state;
+    }
+
+}
 
 const SquareScreen = () => {
-    const CHANGE_RGB_COLOR = 15;
-    const [red, setRed] = useState(0)
-    const [blue, setBlue] = useState(0)
-    const [green, setGreen] = useState(0)
-
-    const setColor = (color, change) => {
-        switch (color) {
-            case 'red':
-                console.log(red);
-                red + change > 255 || red + change < 0 ? null : setRed(red + change)
-                return;
-            case 'blue':
-                console.log(blue);
-                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
-                return;
-            case 'green':
-                console.log(green);
-                green + change > 255 || green + change < 0 ? null : setGreen(green + change)
-                return;
-
-            default:
-                break;
-        }
-
-    }
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 })
+    const { red, green, blue } = state;
+    console.log(state);
     return (
         <View>
             <ColorCounter
                 color="Red"
-                onIncrease={() => setColor('red', CHANGE_RGB_COLOR)}
-                onDecrease={() => setColor('red', -1 * CHANGE_RGB_COLOR)} />
+                onIncrease={() => dispatch({ colorToChange: 'red', amount: CHANGE_RGB_COLOR })}
+                onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * CHANGE_RGB_COLOR })} />
             <ColorCounter color="Blue"
-                onIncrease={() => { setColor('blue', CHANGE_RGB_COLOR) }}
-                onDecrease={() => { setColor('blue', -1 * CHANGE_RGB_COLOR) }}
+                onIncrease={() => dispatch({ colorToChange: 'blue', amount: CHANGE_RGB_COLOR })}
+                onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * CHANGE_RGB_COLOR })}
             />
             <ColorCounter color="Green"
-                onIncrease={() => { setColor('green', CHANGE_RGB_COLOR) }}
-                onDecrease={() => { setColor('green', -1 * CHANGE_RGB_COLOR) }}
+                onIncrease={() => dispatch({ colorToChange: 'green', amount: CHANGE_RGB_COLOR })}
+                onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * CHANGE_RGB_COLOR })}
             />
             <View style={{ height: 100, width: 100, backgroundColor: `rgb(${red},${green},${blue})` }} ></View>
         </View>
