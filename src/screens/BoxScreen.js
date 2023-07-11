26b51289664966b1c1ccd65b239d4cb7c40e1214
Fredrik93 <fredrikullman93@gmail.com>
2023-07-11 11:24:16 +0200

    align items

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,7 +4,10 @@ import { Text, View, StyleSheet } from 'react-native'
 const BoxScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle} >  Box Screen </Text>
+            <Text style={styles.textStyle} >  Box  1</Text>
+            <Text style={styles.textStyle} >  Box  2</Text>
+            <Text style={styles.textStyle} >  Box  3 </Text>
+
         </View>
     )
 }
@@ -12,12 +15,17 @@ const BoxScreen = () => {
 const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 3,
-        borderColor: 'black'
+        borderColor: 'black',
+        height: 200,
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center'
+
     },
     textStyle: {
-        borderWidth: 10,
+        borderWidth: 2,
         borderColor: 'red',
-        margin: 20
+        margin: 5
     }
 })
 export default BoxScreen
